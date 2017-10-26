b140af66ab8384d9cc4d17fe67a0a08d4e18a914
Ryan Do <falcsmike7@gmail.com>
2017-10-26 03:09:55 +0000

    automatically backed up by learn

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -9,4 +9,14 @@ class Cart < ActiveRecord::Base
     end
     total
   end
+
+  def add_item(item_id)
+    line_item = self.line_items.find_by(item_id: item_id)
+    if line_item
+      line_item.quantity += 1
+      line_item.save
+      line_item
+    else
+      self.line_items.build(item_id: item_id)
+  end
 end
