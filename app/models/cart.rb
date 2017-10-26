64844d1fb1169ec2c448311ff98b62d4731c5030
Ryan Do <falcsmike7@gmail.com>
2017-10-26 03:08:12 +0000

    automatically backed up by learn

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,12 @@
 class Cart < ActiveRecord::Base
+  has_many :line_items
+  has_many :items, through: :line_items
 
+  def total
+    total = 0
+    self.line_items.each do |line_item|
+      total += (line_item.quantity * line_item.item.price)
+    end
+    total
+  end
 end
