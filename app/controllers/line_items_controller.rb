bcc76b7afb66f82942405d0620dfdb609bc4e479
Ryan Do <falcsmike7@gmail.com>
2017-10-26 03:38:16 +0000

    automatically backed up by learn

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -4,11 +4,11 @@ class LineItemsController < ApplicationController
     if !cart
       cart = current_user.create_current_cart
       current_user.save
-      cart.add_item(params[:item_id)])
+      cart.add_item(params[:item_id])
       cart.total
       cart.save
     else
-      cart.add_item(params[:item_id)])
+      cart.add_item(params[:item_id])
       cart.total
       cart.save
     end
