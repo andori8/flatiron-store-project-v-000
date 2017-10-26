a39b3ac6405e24be5916c9a69a336ea35a8af989
Ryan Do <falcsmike7@gmail.com>
2017-10-26 03:28:24 +0000

    automatically backed up by learn

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,6 +4,9 @@ class CartsController < ApplicationController
   end
 
   def checkout
-
+    current_user.update(current_cart: nil)
+    @current_cart = Cart.find(params[:id])
+    @current_cart.checkout
+    redirect_to cart_path(@current_cart)
   end
 end
